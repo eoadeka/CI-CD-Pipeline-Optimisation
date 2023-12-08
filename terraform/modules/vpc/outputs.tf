449efaf6969c9777c30ae10166938e721f5c1264
Emmanuella Adeka <onyiadeka@gmail.com>
2023-12-08 16:36:04 +0000

    feat: Expose subnet and security group data

---
+++ terraform/modules/vpc/outputs.tf
@@ -0,0 +1,20 @@
+output "vpc_id" {
+  value = module.vpc.id
+}
+
+output "vpc_public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "vpc_private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "security_group_public" {
+  value = aws_security_group.public.id
+}
+
+output "security_group_private" {
+  value = aws_security_group.private.id
+}
+
