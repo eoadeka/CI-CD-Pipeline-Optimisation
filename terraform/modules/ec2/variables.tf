de582ec167ff5008aeb33c78b06e412100dcee08
Emmanuella Adeka <onyiadeka@gmail.com>
2023-12-09 17:44:02 +0000

    Add input variables for EC2 module

---
+++ terraform/modules/ec2/variables.tf
@@ -0,0 +1,40 @@
+variable "environ" {
+  type = string
+  description = "Infrastructure environment"
+}
+
+variable "project" {
+  type = string
+  default = "cicdpo.io"
+}
+
+variable "tags" {
+    type = map(string)
+    default = {}
+    description = "..."
+}
+
+variable "create_eip" {
+  type = bool
+  default = bool
+  description = "create EIP or not?"
+}
+
+variable "instance_ami" {
+  type = string
+  description = "AMI of EC2 instance"
+}
+
+variable "instance_type" {
+  type = string
+  description = "Instance type of EC2 instance"
+}
+
+variable "subnets" {
+  type = list(string)
+}
+
+variable "security_groups" {
+    type = list(string)
+  default = []
+}
