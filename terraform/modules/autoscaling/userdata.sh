5ebab282b21df2c0168a2701b09bd9a75ea77253
Emmanuella Adeka <onyiadeka@gmail.com>
2024-01-03 00:54:43 +0000

    fix: Fix user data script

--- terraform/modules/autoscaling/userdata.sh
+++ terraform/modules/autoscaling/userdata.sh
@@ -10,11 +10,27 @@ sudo yum update -y
 
 # Install new packages
 sudo yum install -y httpd git python3 python3-pip python3-devel virtualenv
+
+# Verify python installation by checking version
 sudo python3 --version
+
+# Enable and start Apache (httpd)
 sudo systemctl enable httpd
-git clone https://github.com/ella-adeka/CI-CD-Pipeline-Optimisation.git /var/www/html/web/my_app/
 sudo systemctl start httpd
-sudo cd my_app/
+
+# Clone Github repository
+sudo git clone https://github.com/ella-adeka/CI-CD-Pipeline-Optimisation.git /var/www/html/web/
+
+# Navigate to app directory
+sudo cd /var/www/html/web/my_app/
+
+# Create virtual environment and install dependencies
+sudo python3 -m venv venv
+source venv/bin/activate
 sudo python3 -m pip install -r requirements.txt
+
+# Export flask app environment variable
 export FLASK_APP = app.py
-flask run
+
+# Run the Flask app
+flask run --host=0.0.0.0 --port=5000
